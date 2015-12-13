64657a0fa4433c39ed3a1e6f6d78653d46cd0207
Montchat <je4440@gmail.com>
2015-12-13 18:33:25 -0500

    added imageView. Working on the sideViews
    - added imageView to display potential login possibilities
    - added gestureRecognizers. work in progress.
    - added sideViews to give the illusion that the scrollable view
    stretches. also a work in progress

--- ScrollableViews/ViewController.swift
+++ ScrollableViews/ViewController.swift
@@ -16,32 +16,102 @@ class ViewController: UIViewController {
 
     //MARK: @IBOutlets
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var submarineImageView: UIImageView!
+    
+    //MARK: GestureRecognizers - Outlets for setting delegates
+    @IBOutlet var swipeGestureRecognizer: UISwipeGestureRecognizer!
+    @IBOutlet var submarineGestureRecognizer: UILongPressGestureRecognizer!
+    
+    //Actions for telling the swipeGestureRecognizers what to do
+    @IBAction func swipeGestureRecognizerAction(swipe: UISwipeGestureRecognizer) {
+        if (swipe.state == .Began && swipe.direction == .Left) || (swipe.state == .Began && swipe.direction == .Right) {
+            changeAlpha(submarineImageView, alpha: 0.50)
+            
+        }
+        
+        if swipe.state == .Ended {
+            changeAlpha(submarineImageView, alpha: 1)
+            
+        }
+        
+    }
+    
+    @IBAction func submarineGestureReconizerAction(press: UILongPressGestureRecognizer) {
+        if press.state == .Began {
+            changeAlpha(submarineImageView, alpha: 0.50)
+        }
+        if press.state == .Ended {
+            changeAlpha(submarineImageView, alpha: 1)
+
+        }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //Creating our three viewControllers that will each contain their own view
-        let view1 = UIViewController(nibName: VIEW1,
+        let loginVC = UIViewController(nibName: VIEW1,
             bundle: nil)
-        let view2 = UIViewController(nibName: VIEW2, bundle: nil)
-        let view3 = UIViewController(nibName: VIEW3, bundle: nil)
+        let mainVC = UIViewController(nibName: VIEW2, bundle: nil)
+        let signUpVC = UIViewController(nibName: VIEW3, bundle: nil)
         
-        //Adding our views to the viewControllers
-        scrollView.addSubview(view1.view)
-        scrollView.addSubview(view2.view)
-        scrollView.addSubview(view3.view)
+        //Makes it look like the loginVC and the signUp VC are "stretchy"
+        let greenSide = UIViewController(nibName: VIEW1, bundle: nil)
+        let redSide = UIViewController(nibName: VIEW3, bundle: nil)
+
+        //SideViews
+        greenSide.view.frame.origin.x = view.frame.origin.x
+        redSide.view.frame.origin.x = view.frame.midX
+        view.addSubview(greenSide.view)
+        view.addSubview(redSide.view)
+        
+        //MainViews
+        scrollView.addSubview(loginVC.view)
+        scrollView.addSubview(mainVC.view)
+        scrollView.addSubview(signUpVC.view)
+        
+        view.bringSubviewToFront(scrollView)
+        view.bringSubviewToFront(submarineImageView)
         
         //Laying out the different views in the scrollView
-        view2.view.frame.origin.x = view.frame.width
-        view3.view.frame.origin.x = view.frame.width * 2
+        mainVC.view.frame.origin.x = view.frame.width
+        signUpVC.view.frame.origin.x = view.frame.width * 2
         
         //Adjusting the size of the scrollViews
         scrollView.contentSize = CGSize(width: view.frame.width * 3, height: view.frame.height)
+        //Set the views so that the main view is in the middle and the login and signUp are on the left and right respectively.
+        scrollView.setContentOffset(mainVC.view.frame.origin, animated: false)
         
+        //Setting the delegates of the GestureRecoginzers above
+        swipeGestureRecognizer.delegate = self
+        submarineGestureRecognizer.delegate = self
+
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     
     }
+    
+}
 
+extension ViewController : UIGestureRecognizerDelegate {
+    //creating a function to set gesture recognizer delegates
+    func setDelegate(gestureRecognizer: UIGestureRecognizer) {
+        gestureRecognizer.delegate = self
+        
+    }
+    
+}
+
+extension ViewController {
+    //MARK: Functions
+    func changeAlpha(view:UIView, alpha: Double) {
+        UIView.animateWithDuration(0.33, animations: { () -> Void in
+            view.alpha = 0.50
+            
+        })
+        
+    }
+    
 }
